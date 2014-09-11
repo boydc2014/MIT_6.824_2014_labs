97e5c5dce8a29d1c9f103de7199b1d52d7e32323
Lei Dong <leidong@yahoo-inc.com>
2014-09-11 18:08:27 +0800

    Lab1: Part I word count

--- src/main/wc.go
+++ src/main/wc.go
@@ -5,14 +5,43 @@ import "fmt"
 import "mapreduce"
 import "container/list"
 
+import "unicode"
+import "strings"
+import "strconv"
+
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+    // split into words
+    f := func(c rune) bool {
+        return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+    }
+    words := strings.FieldsFunc(value, f)
+
+    // count word
+    counts := make(map[string]int)
+    for _ , word := range words {
+        counts[word] +=  1
+    }
+
+    // form output
+    kvList := list.New()
+    for word, count := range counts {
+        kv := mapreduce.KeyValue{word, strconv.Itoa(count)}
+        kvList.PushBack(kv)
+    }
+    return kvList
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+    countTotal := 0
+    for item := values.Front(); item != nil; item = item.Next() {
+        count, _ := strconv.Atoi(item.Value.(string))
+        countTotal += count
+    }
+    return strconv.Itoa(countTotal)
 }
 
 // Can be run in 3 ways:
